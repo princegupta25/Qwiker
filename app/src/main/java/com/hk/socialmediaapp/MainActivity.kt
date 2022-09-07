117a55fbbbaa4b9893cb4c7e20341ac5627326d9
princegupta25 <princesmart.gupta@gmail.com>
2022-09-07 22:48:01 +0530

    Authentication

--- app/src/main/java/com/hk/socialmediaapp/MainActivity.kt
+++ app/src/main/java/com/hk/socialmediaapp/MainActivity.kt
@@ -3,17 +3,87 @@ package com.hk.socialmediaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
-import com.hk.socialmediaapp.loginandsignup.LoginSignupActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.hk.UI.CreateFragment
+import com.hk.UI.FeedFragment
+import com.hk.UI.ProfileFragment
+import com.hk.socialmediaapp.databinding.ActivityMainBinding
+import com.hk.socialmediaapp.databinding.AddBottomSheetBinding
+import com.hk.socialmediaapp.post.CreateImagePost
+import com.hk.socialmediaapp.post.CreateTextPost
 
 class MainActivity : AppCompatActivity() {
+
+    private val createFragment = CreateFragment()
+    private val feedFragment = FeedFragment()
+    private val profileFragemnt = ProfileFragment()
+
+    private lateinit var sheetBinding: AddBottomSheetBinding
+    lateinit var bottomSheetDialog: BottomSheetDialog
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.startBtn)
-        btn.setOnClickListener {
-            startActivity(Intent(this,LoginSignupActivity::class.java))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sheetBinding = AddBottomSheetBinding.bind(
+            LayoutInflater.from(this).inflate(R.layout.add_bottom_sheet,null)
+        )
+        bottomSheetDialog = BottomSheetDialog(
+            this,R.style.BottomSheetDialogTheme
+        )
+
+        replaceFragment(feedFragment)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.feed-> replaceFragment(feedFragment)
+                R.id.create-> showBottomSheet()
+                R.id.profile-> replaceFragment(profileFragemnt)
+            }
+            true
+        }
+
+
+
+    }
+
+    private fun showBottomSheet() {
+        val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
+            R.layout.add_bottom_sheet,
+            findViewById<ConstraintLayout>(R.id.addBottomSheetId)
+        )
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
+
+        bottomSheetDialog.findViewById<Button>(R.id.uploadText)!!.setOnClickListener {
+            startActivity(Intent(this,CreateTextPost::class.java)
+                .putExtra("Post_Type","TextPost"))
+
+        }
+
+        bottomSheetDialog.findViewById<Button>(R.id.uploadImage)!!.setOnClickListener {
+            startActivity(Intent(this,CreateImagePost::class.java)
+                .putExtra("Post_Type","ImagePost"))
+
+        }
+
+    }
+
+
+    fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container,fragment)
+            transaction.commit()
         }
     }
+
 }
